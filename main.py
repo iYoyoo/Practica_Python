0f3bc10b6853324178a3e87d2e3daa3ea0384ba6
Rodrigo Vergara <rodrigo.vegara.ctrabajo@gmail.com>
2023-04-04 22:26:44 -0400

    Actualización de Código
    Se avanzó en el código de la tarea 1 de python.

--- main.py
+++ main.py
@@ -8,9 +8,49 @@ let miOtraVariable;"""
 # mi_variable = "Rodrigo"
 # mi_Edad = 28
 
-#nombre = input("Ingrese su nombre")
-#print("Bienvenido" + nombre)
+# nombre = input("Ingrese su nombre")
+# print("Bienvenido" + nombre)
 
-edad = str(input("Ingrese su edad: "))
-edad_aumentada = edad + 1
-#print ("Usted tiene " + edad + " años." + "y el próximo cumpleaños usted tendrá: " + str(edad_aumentada))print (f"Usted tiene " + edad + " años." + "y el próximo cumpleaños usted tendrá: " {edad_aumentada})
+# edad = str(input("Ingrese su edad: "))
+# edad_aumentada = edad + 1
+# print ("Usted tiene " + edad + " años." + "y el próximo cumpleaños usted tendrá: " + str(edad_aumentada))
+# print (f"Usted tiene " + edad + " años." + "y el próximo cumpleaños usted tendrá: " {edad_aumentada})
+
+productoReloj = 10000
+productoCollar = 11000
+productoBrazalete = 12000
+productoAnillo = 13000
+productoAros = 14000
+
+conjuntoProductos = [productoReloj, productoCollar,
+                     productoBrazalete, productoAnillo, productoAros]
+cantidadProductos = input(
+    "Ingrese: \n 0. Seleccionar Reloj \n 1. Seleccionar Collar \n 2. Seleccionar Brazalete \n 3. Seleccionar Anillo \n 4. Seleccionar Aros \n : ")
+if int(cantidadProductos) == 0:
+    print("Selecciono Reloj")
+    cantidadCompra = input("Ingrese cantidad a llevar: ")
+    print("Total: " + "$" + str(int(productoReloj) * int(cantidadCompra)))
+
+else:
+    if int(cantidadProductos) == 1:
+        print("Selecciono Collar")
+        cantidadCompra = input("Ingrese cantidad a llevar: ")
+        print("Total: " + "$" + str(int(productoReloj) * int(cantidadCompra)))
+
+    else:
+        if int(cantidadProductos) == 2:
+            print("Selecciono Brazalete")
+            cantidadCompra = input("Ingrese cantidad a llevar: ")
+            print("Total: " + "$" + str(int(productoReloj) * int(cantidadCompra)))
+        else:
+            if int(cantidadProductos) == 3:
+                print("Selecciono Anillo")
+                cantidadCompra = input("Ingrese cantidad a llevar: ")
+                print("Total: " + "$" +
+                      str(int(productoReloj) * int(cantidadCompra)))
+            else:
+                if int(cantidadProductos) == 4:
+                    print("Selecciono Aros")
+                    cantidadCompra = input("Ingrese cantidad a llevar: ")
+                    print("Total: " + "$" +
+                          str(int(productoReloj) * int(cantidadCompra)))
