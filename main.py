8fb081471bc6c3a9436f665c9349ba618f996772
Rodrigo Vergara <rodrigo.vegara.ctrabajo@gmail.com>
2023-04-04 21:14:01 -0400

    Código de Práctica
    Tratar de entender sintaxis hablada en clases

---
+++ main.py
@@ -0,0 +1,16 @@
+"""<!-- Comentario --> / html
+// Comentario / CSS
+/* Comentario / Js """
+
+"""var miVariable;
+let miOtraVariable;"""
+
+# mi_variable = "Rodrigo"
+# mi_Edad = 28
+
+#nombre = input("Ingrese su nombre")
+#print("Bienvenido" + nombre)
+
+edad = str(input("Ingrese su edad: "))
+edad_aumentada = edad + 1
+#print ("Usted tiene " + edad + " años." + "y el próximo cumpleaños usted tendrá: " + str(edad_aumentada))print (f"Usted tiene " + edad + " años." + "y el próximo cumpleaños usted tendrá: " {edad_aumentada})
