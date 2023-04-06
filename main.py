814692975c07ac4ee3bb45ad6585b33485ac1c68
Rodrigo Vergara <rodrigo.vegara.ctrabajo@gmail.com>
2023-04-05 22:48:06 -0400

    Cambios en el código
    Seba se metió a ayudarme con el código.

--- main.py
+++ main.py
@@ -23,37 +23,47 @@ productoAnillo = 13000
 productoAros = 14000
 
 
+valorSeleccionado = 0
 conjuntoProductos = [productoReloj, productoCollar,
                      productoBrazalete, productoAnillo, productoAros]
-cantidadProductos = input(
-    "Ingrese: \n 0. Seleccionar Reloj \n 1. Seleccionar Collar \n 2. Seleccionar Brazalete \n 3. Seleccionar Anillo \n 4. Seleccionar Aros \n : ")
-if int(cantidadProductos) == 0:
-    print("Selecciono Reloj")
-    cantidadCompra = input("Ingrese cantidad a llevar: ")
-else:
-    if int(cantidadProductos) == 1:
-        print("Selecciono Collar")
-        cantidadCompra = input("Ingrese cantidad a llevar: ")
-    else:
-      if int(cantidadProductos) == 2:
-        print("Selecciono Brazalete")
-        cantidadCompra = input("Ingrese cantidad a llevar: ")
-      else:
-        if int(cantidadProductos) == 3:
-          print("Selecciono Anillo")
+sigueComprando = True;
+
+while(sigueComprando): 
+  producto = input(
+      "Ingrese: \n 0. Seleccionar Reloj \n 1. Seleccionar Collar \n 2. Seleccionar Brazalete \n 3. Seleccionar Anillo \n 4. Seleccionar Aros \n : ")
+  if int(producto) == 0:
+      print("Selecciono Reloj")
+      cantidadCompra = input("Ingrese cantidad a llevar: ")
+      valorSeleccionado = productoReloj
+  else:
+      if int(producto) == 1:
+          print("Selecciono Collar")
           cantidadCompra = input("Ingrese cantidad a llevar: ")
+          valorSeleccionado = productoCollar
+      else:
+        if int(producto) == 2:
+          print("Selecciono Brazalete")
+          cantidadCompra = input("Ingrese cantidad a llevar: ")
+          valorSeleccionado = productoBrazalete
         else:
-          if int(cantidadProductos) == 4:
-            print("Selecciono Aros")
+          if int(producto) == 3:
+            print("Selecciono Anillo")
             cantidadCompra = input("Ingrese cantidad a llevar: ")
-          
-cantidad_Productos = int(cantidadProductos)
-cantidad_Compra = int(cantidadCompra)
-totalCompra = cantidad_Compra*cantidad_Productos
+            valorSeleccionado = productoAnillo
+          else:
+            if int(producto) == 4:
+              print("Selecciono Aros")
+              cantidadCompra = input("Ingrese cantidad a llevar: ")
+              valorSeleccionado = productoAros
+            
+  productoSeleccionado = int(producto)
+  cantidadSeleccionada = int(cantidadCompra)
+  totalCompra = cantidadSeleccionada*valorSeleccionado
+  print("El total neto de su compra es: $ " + str(totalCompra))
+  respuestaSeguirComprando = str(input("¿Desea agregar algo más?(Si/No) :"))
+  if (respuestaSeguirComprando.lower() == "si"):
+     sigueComprando = True
+  else :
+    sigueComprando = False
 
-print("El total neto de su compra es: $ " + str(totalCompra))
-
-respuestaUsuario = str(input("¿Desea agregar algo más?(Si/No) :"))
-if respuestaUsuario == ("Si" or "si"):
-  cantidadProductos;
-  cantidadCompra;
+print("Gracias por comprar")
